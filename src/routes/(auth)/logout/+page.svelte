6c5b13c30ccad6e61e5e355db50e133c1c1a3f24
Vladislav Smelko <Vladislav.smelko97@gmail.com>
2023-11-22 15:11:19 +0200

    fix: responsive logout

---
+++ src/routes/(auth)/logout/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+	import { browser } from '$app/environment';
+	export let data;
+
+	onMount(() => {
+		if (browser && data.success)
+			goto('/', {
+				invalidateAll: true,
+			});
+	});
+</script>
