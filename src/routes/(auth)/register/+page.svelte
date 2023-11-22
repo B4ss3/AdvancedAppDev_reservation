8efb03d6d2c1807c0840f01436b2539815fb45ad
Vladislav Smelko <Vladislav.smelko97@gmail.com>
2023-11-22 15:14:43 +0200

    fix: responsive register

--- src/routes/(auth)/register/+page.svelte
+++ src/routes/(auth)/register/+page.svelte
@@ -8,7 +8,7 @@
 		resetForm: true,
 		onResult: async ({ result }) => {
 			if (result.type === 'success') {
-				await goto('/login');
+				await goto('/login', { invalidateAll: true });
 			} else {
 				console.error(result.error);
 			}
