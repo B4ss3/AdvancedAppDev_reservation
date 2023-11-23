571fc010e945232ace859b3363c6270af5e2c615
kripi-png <66375160+kripi-png@users.noreply.github.com>
2023-11-23 09:12:43 +0200

    Main page styling (#14)
    * style: layout / navigation bar styling
    
    * feat: styling for the main page (all apartments)
    
    * feat: message field
    
    * feat: description field and general changes
    
    - description field
    - change type to apartmentType
    - use enum as apartmentType, import from `$lib/constants`
    - use said enum in apartment creation form to generate list of options 
    using #each
    
    * feat: display rent per month and Rent button
    
    - add page for application form

---
+++ src/lib/constants.js
@@ -0,0 +1,5 @@
+export const APARTMENT_TYPES = {
+	ONE_ROOM_FLAT: 'One room flat',
+	TWO_ROOM_FLAT: 'Two room flat',
+	THREE_ROOM_FLAT: 'Three room flat',
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,112 @@
 <script>
+	import { APARTMENT_TYPES } from '$lib/constants';
+	import { goto } from '$app/navigation';
+
 	export let data;
 	console.log(data);
+
+	const pictureLinks = [
+		'https://d2ue5ppt0wsjaa.cloudfront.net/640x427,fit/vuokraovimedia/images/162/823/712/412/162823712412664_original.jpg',
+		'https://d2ue5ppt0wsjaa.cloudfront.net/640x427,fit/vuokraovimedia/images/170/064/956/491/170064956491299_original.jpg',
+		'https://d2ue5ppt0wsjaa.cloudfront.net/640x427,fit/vuokraovimedia/images/167/403/220/573/167403220573367_original.jpg',
+		'https://kumppanisisallot.fi/etuovicom/wp-content/uploads/sites/18/2023/06/Torkkelinmaki_101459.jpg',
+	];
+
+	const getRandomPicture = () => {
+		const r = Math.floor(Math.random() * pictureLinks.length);
+		return pictureLinks[r];
+	};
+
+	const constructAddress = (apartment) => {
+		let { cityName, postalCode, streetName, apartmentNumber } = apartment;
+		// cannot default in deconstruct if value is null
+		if (cityName === null) cityName = 'Turku';
+		if (postalCode === null) postalCode = '20540';
+		if (streetName === null) streetName = 'Jaanintie 34';
+		if (apartmentNumber === null) apartmentNumber = 'D 72';
+
+		return `${streetName} ${apartmentNumber}, ${postalCode}, ${cityName}`;
+	};
+
+	const convertType = (rawType) => {
+		return APARTMENT_TYPES[rawType] || undefined;
+	};
+
+	const getRoomString = (apartment) => {
+		// returns a string representation of room counts
+		// e.g. 1 h + kt + s + kh + s
+		const {
+			roomNormalCount,
+			roomKitchenCount,
+			roomBalconyCount,
+			roomBathroomCount,
+		} = apartment;
+
+		let roomStrings = [];
+		if (roomNormalCount > 0) roomStrings.push(`${roomNormalCount} h`);
+		if (roomKitchenCount > 0) roomStrings.push(`${roomKitchenCount} kt`);
+		if (roomBathroomCount > 0) roomStrings.push(`${roomBathroomCount} kh`);
+		if (roomBalconyCount > 0) roomStrings.push(`${roomBalconyCount} p`);
+
+		return roomStrings.join(' + ');
+	};
 </script>
 
-<h1>insert here : front (view all apartments, no sorting)</h1>
-<ul>
-	{#each data.apartments as apartment}
-		<li><a href={'/apartments/' + apartment.id}>{apartment.name}</a></li>
-	{/each}
-</ul>
+{#await data.apartments}
+	<p>Waiting...</p>
+{:then apartments}
+	<div class="grid grid-cols-4 gap-4">
+		{#each apartments as apartment}
+			{@const roomString = getRoomString(apartment) || null}
+			<div class="card .variant-glass-primary card-hover overflow-hidden">
+				<header>
+					<img
+						src={getRandomPicture()}
+						class="bg-black/50 w-full aspect-[21/9]"
+						alt={apartments.address}
+					/>
+				</header>
+				<div class="p-4 space-y-4">
+					<h6 class="h6 block">
+						{constructAddress(apartment)}
+						<br />
+						<span>{apartment.area} m²</span>
+						{#if roomString}
+							<span>&nbsp;({roomString})</span>
+						{/if}
+					</h6>
+
+					<article>
+						<p>
+							{#if apartment.description}
+								{apartment.description}
+							{:else}
+								No description
+							{/if}
+						</p>
+					</article>
+				</div>
+				<hr class="opacity-50" />
+				<footer class="p-4 flex justify-start items-center space-x-4">
+					<div class="flex-auto flex justify-between items-center">
+						<h6
+							class="font-bold"
+							data-toc-ignore
+						>
+							By {apartment.ownerId}
+						</h6>
+						<h4 class="h4 ml-auto mr-2">{apartment.rentAmount} €/kk</h4>
+						<button
+							type="button"
+							class="btn variant-filled"
+							on:click={async () =>
+								await goto(`/apartments/${apartment.apartmentId}/apply`)}
+						>
+							<span>Rent</span>
+						</button>
+					</div>
+				</footer>
+			</div>
+		{/each}
+	</div>
+{/await}
