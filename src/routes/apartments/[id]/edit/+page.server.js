2d88e8b14800912d16169ce9f857052f097a5f93
kripi-png <66375160+kripi-png@users.noreply.github.com>
2023-11-29 17:20:48 +0200

    fix: apartment edit form uses applications schema

--- src/routes/apartments/[id]/edit/+page.server.js
+++ src/routes/apartments/[id]/edit/+page.server.js
@@ -1,12 +1,14 @@
 import { fail, redirect } from '@sveltejs/kit';
 import { superValidate } from 'sveltekit-superforms/server';
-import applicationSchema from '$lib/schemas/application';
+import apartmentSchema from '$lib/schemas/apartment';
 
 export const load = async ({ locals, fetch, params }) => {
 	if (!locals.user) throw redirect(307, '/login');
 
-
-
-	const form = await superValidate(applicationSchema);
+	// fetch application entry for the id
+	const response = await fetch(`http://localhost:8080/apartments/${params.id}`);
+	// validate application data and populate edit form
+	const data = await response.json();
+	const form = await superValidate(data, apartmentSchema);
 	return { form };
 };
